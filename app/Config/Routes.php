504ea46951423b37b78c9b0710a240862ea3b9d0
Anthonyyy29 <adiezraanthoni@gmail.com>
2026-01-03 10:15:22 +0700

    Initial commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::contact');
+$routes->get('/faqs', 'Page::faqs');
+$routes->get('/faqs', 'Page::tos');
+$routes->get('/news', 'News::index');
+
+
+$routes->setAutoRoute(false);
